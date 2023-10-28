52ccfe098437fa16758f01ce11bd5aaa66422c39
cris10988 <135299699+cris10988@users.noreply.github.com>
2023-10-27 22:02:57 -0500

    Add files via upload
    actualizado

--- pindex.js
+++ pindex.js
@@ -1,15 +1,16 @@
 /**console.log ('Hola mundo');**/
 import express from 'express';
 import mongoose from 'mongoose';
+import routers from './routers/procesoRouters.js';
 
 const server = express();
 const PORT = 3000;
 
 server.use(express.json());
-
+server.use(routers);
 async function main() {
     try {
-        await mongoose.connect('mongodb+srv://crispi:la10@cluster0.cv3bpug.mongodb.net/');
+        await mongoose.connect('mongodb+srv://crispi:la10@cluster0.cv3bpug.mongodb.net/proyeSterilization');
         
         server.listen(PORT, () => {
             console.log(`Server up in http://localhost:${PORT}`);
